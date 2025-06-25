ce9054809b14dc6cb90a86faf8b13dd090aa609c
moen <moenr49@gmail.com>
2025-06-25 21:39:20 +0300

    final  commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here!// Remove the <main> with id 'main'
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new header
+newHeader.id = 'victory';
+
+// Set the text inside the new header
+newHeader.textContent = 'Moen is the champion';
+
+// Add the header to the document body
+document.body.appendChild(newHeader);
